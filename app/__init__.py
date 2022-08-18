4e05de05ff4781fdfe45cecbfc7f19de4b6ff2c6
Shayan Azizi <lipbiriom@gmail.com>
2022-08-18 12:15:09 +0430

    Add errors to signup page and add db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
+
+db = SQLAlchemy()
+database_name = "database.db"
+
 
 def create_app ():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + database_name
+    
+    db.init_app(app)
+    
+    
     
     from .views import views
     from .auth import auth
--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,59 @@
-from flask import blueprints, render_template
+from flask import Blueprint, render_template, request, flash
 
-auth = blueprints.Blueprint("auth", __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+digits = "0123456789"
+letters = "abcdefghijklmnopqrstuvwxyz"
+special_characters = "!@#$%^&*()_+"
+
+def validate_password (password):
+    
+    for i in password:
+        if i in digits or i in letters or i in special_characters:
+            return True
+        
+    return False
+
+@auth.route("/login", methods = ["GET", "POST"])
 def login ():
-    return render_template ("login.html")
+
+    return render_template ("login.html", test = "salam")
 
 @auth.route("/logout")
 def logout ():
     return "You logged out"
 
-@auth.route("/signup")
+@auth.route("/signup", methods = ["GET", "POST"])
 def signup ():
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+        
+        if password != confirm_password:
+            flash ("رمز ها همخوانی ندارند.", category="error")
+        
+        elif len (username) < 3:
+            flash ("نام کاربری باید حداقل 3 کاراکتر باشد.", category="error")
+            
+        elif len (password) < 8:
+            flash ("رمز ورود باید حداقل 8 کاراکتر باشد.", category="error")
+            
+        elif len (username) > 32:
+            flash("نام کاربری باید کمتر از 32 کاراکتر باشد.", category="error")
+            
+        elif len (password) > 50:
+            flash("رمز عبور باید کمتر از 50 کاراکتر باشد.", category="error")
+        
+        
+        elif not validate_password (password) :
+            flash("رمز ورود باید حداقل از یک عدد، حرف بزرگ یا کوچک و یک کاراکتر خاص پشتیبانی کند.", category="error")
+        
+        else:
+            flash("حساب کاربری شما ایجاد شد.", category="success")
+    
     return render_template ("signup.html")
